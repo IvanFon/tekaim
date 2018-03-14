619a86ff25afd535b9b9c05975927019ea2ba98f
Ivan Fonseca <ivanfon@riseup.net>
2018-03-13 21:59:06 -0400

    Copy file url to clipboard

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='tekaim',
-    version='1.0.1',
+    version='1.1.0',
     description='A simple tool to take and upload screenshots to teknik.io',
     author='Ivan Fonseca',
     author_email='ivanfon@riseup.net',
